b7e9ece34261d14fe1146d86bb05d61aa7cb1376
Nasim Reza <nasim8382@gmail.com>
2022-04-17 15:51:27 +0600

    active route link added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -3,23 +3,24 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import './Header.css';
 import logo from "../../../images/logo.png";
+import CustomLink from "../CustomLink/CustomLink";
 
 const Header = () => {
   return (
     <>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
         <Container>
-          <Navbar.Brand href="#home">
+          <Navbar.Brand as={Link} to="home">
             <img src={logo} height={40} alt="" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mx-auto">
-              <Nav.Link className="me-5 menu-item" as={Link} to="home">HOME</Nav.Link>
-              <Nav.Link className="me-5 menu-item" as={Link} to="blogs">BLOGS</Nav.Link>
-              <Nav.Link className="me-5 menu-item" as={Link} to="about">ABOUT</Nav.Link>
-              <Nav.Link className="me-5 menu-item" as={Link} to="contact">CONTACT</Nav.Link>
-              <Nav.Link className="me-5 menu-item" as={Link} to="login">LOGIN</Nav.Link>
+                <CustomLink className="me-5 menu-item" as={Link} to="home">HOME</CustomLink>
+                <CustomLink className="me-5 menu-item" as={Link} to="blogs">BLOGS</CustomLink>
+                <CustomLink className="me-5 menu-item" as={Link} to="about">ABOUT</CustomLink>
+                <CustomLink className="me-5 menu-item" as={Link} to="contact">CONTACT</CustomLink>
+                <CustomLink className="me-5 menu-item" as={Link} to="login">LOGIN</CustomLink>
             </Nav>
           </Navbar.Collapse>
         </Container>
