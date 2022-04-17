2c10f4da5b5c64b710a6a07294b8a73bcb07dad9
Nasim Reza <nasim8382@gmail.com>
2022-04-18 02:49:28 +0600

    sign in and sign out integrated and privateroute created

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,11 +1,47 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import login from '../../../images/login.png';
 import SocialLogin from '../SocialLogin/SocialLogin';
 import background from '../../../images/bg.jpg';
 import './Login.css';
+import auth from '../../../firebase.init';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 
 const Login = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    let from = location.state?.from?.pathname || "/";
+    let errorElement;
+    let loadingText;
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error
+    ] = useSignInWithEmailAndPassword(auth);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        signInWithEmailAndPassword(email, password);
+    }
+
+    if (loading) {
+        loadingText = <p className='text-center purple'>Loading...</p>;
+    }
+
+    if (error) {
+        errorElement =  <p className='text-center text-danger mt-2'>Error: {error?.message}</p>
+    }
+
+    if (user) {
+        navigate(from, { replace: true });
+    }
+
     return (
         <div className='container'>
             <div className='row login-div'>
@@ -18,12 +54,15 @@ const Login = () => {
                 style={{backgroundImage: `url(${background})`}}>
                     <h2 className="my-4 text-center form-title">Dear Client Please Sign In</h2>
                     <div className='login-form w-75  mx-auto'>
-                        <form>
+                        <form onSubmit={handleSubmit}>
                             <label htmlFor="email">Email</label>
                             <input className='input-field' type="email" name="email" placeholder='Email Address' required/>
 
                             <label htmlFor="email">Password</label>
                             <input className='input-field' type="password" name="password" placeholder='Your Password' required/>
+
+                            {loadingText}
+                            {errorElement}
 
                             <input className='mx-auto mt-4 mb-3 rounded text-uppercase login-btn' type="submit" value="Login" />
                         </form>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -4,8 +4,16 @@ import { Link } from "react-router-dom";
 import './Header.css';
 import logo from "../../../images/logo.png";
 import CustomLink from "../CustomLink/CustomLink";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
+import { signOut } from "firebase/auth";
 
 const Header = () => {
+  const [user] = useAuthState(auth);
+
+  const handleSignOut = () => {
+    signOut(auth);
+  }
   return (
     <>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
@@ -16,11 +24,15 @@ const Header = () => {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mx-auto">
-                <CustomLink className="me-5 menu-item" as={Link} to="home">HOME</CustomLink>
-                <CustomLink className="me-5 menu-item" as={Link} to="blogs">BLOGS</CustomLink>
-                <CustomLink className="me-5 menu-item" as={Link} to="about">ABOUT</CustomLink>
-                <CustomLink className="me-5 menu-item" as={Link} to="contact">CONTACT</CustomLink>
-                <CustomLink className="me-5 menu-item" as={Link} to="login">SIGN IN</CustomLink>
+                <CustomLink className="me-5 menu-item" to="home">HOME</CustomLink>
+                <CustomLink className="me-5 menu-item" to="blogs">BLOGS</CustomLink>
+                <CustomLink className="me-5 menu-item" to="about">ABOUT</CustomLink>
+                <CustomLink className="me-5 menu-item" to="contact">CONTACT</CustomLink>
+                {
+                  user ?
+                  <button className="btn btn-link text-white text-decoration-none sign-out-btn" onClick={handleSignOut}>SIGN OUT</button> :
+                  <CustomLink className="me-5 menu-item" to="login">SIGN IN</CustomLink>
+                }
             </Nav>
           </Navbar.Collapse>
         </Container>
