37357952169418f50ad62daf1db8fa840f84c20e
Nasim Reza <nasim8382@gmail.com>
2022-04-17 14:43:30 +0600

    register page added

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -8,7 +8,7 @@ import './Register.css';
 const Register = () => {
     return (
         <div className='container'>
-            <div className='row login-div'>
+            <div className='row register-div'>
 
                 <div className='col-12 col-md-6 img-div'>
                     <img className='img-fluid' src={register} alt="" />
@@ -17,7 +17,7 @@ const Register = () => {
                 <div className='col-12 col-md-6 my-auto py-4 form-div'
                 style={{backgroundImage: `url(${background})`}}>
                     <h2 className="my-2 text-center">Welcome You</h2>
-                    <div className='login-form w-75  mx-auto'>
+                    <div className='register-form w-75  mx-auto'>
                         <form>
                             <label htmlFor="name">Name</label>
                             <input className='input-field' type="text" name="name" placeholder='Your Name'/>
@@ -28,7 +28,10 @@ const Register = () => {
                             <label htmlFor="email">Password</label>
                             <input className='input-field' type="password" name="password" placeholder='Your Password' required/>
 
-                            <input className='mx-auto mt-4 mb-3 rounded text-uppercase login-btn' type="submit" value="Login" />
+                            <input className='me-2' type="checkbox" name="terms" />
+                            <label htmlFor="terms">Agree to terms and Conditions</label>
+
+                            <input className='mx-auto mt-2 mb-3 rounded text-uppercase register-btn' type="submit" value="Register" />
                         </form>
                         <p className='text-center'>have an Account?
                             <Link to='/login' className="orange-text text-decoration-none"> Login</Link>
