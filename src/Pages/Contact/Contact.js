c2b62b8dfa1469164a273854472405a1c45f3e1e
Nasim Reza <nasim8382@gmail.com>
2022-04-18 23:06:56 +0600

    contact page updated

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import logo from '../../images/logo.png';
+import enjoy from '../../images/enjoy.jpg';
+import './Contact.css';
 
 const Contact = () => {
     return (
-        <div>
-            <h2>Contact us</h2>
+        <div className='container contact'>
+            <div className='row'>
+                <div className='col-12 col-md-6'>
+                    <img className='img-fluid contact-img' src={enjoy} alt="" />
+                </div>
+                <div className='col-12 col-md-6'>
+                    <div className='contact-div'>
+                        <img src={logo} alt="" />
+                        <h2>Contact</h2>
+                        <h5>Address: Gangni, Meherpur</h5>
+                        <h5>Contact No: +88017100005555</h5>
+                        <h5>Enail: be-photography@gmail.com</h5>
+                        <h5>Website: www.be-photography.com</h5>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
